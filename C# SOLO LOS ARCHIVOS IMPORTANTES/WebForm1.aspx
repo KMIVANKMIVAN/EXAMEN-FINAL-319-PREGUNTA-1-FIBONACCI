088b996a8922eb7f092d7fd5caa618b5b03d6cd2
KMIVANKMIVAN <88416056+KMIVANKMIVAN@users.noreply.github.com>
2021-12-14 22:50:00 -0400

    Add files via upload

---
+++ C# SOLO LOS ARCHIVOS IMPORTANTES/WebForm1.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="FIBONA.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <a>DIGITE DE QUE NUMERO QUIERE LA SERIE FIBONACCI TIENE QUE SER SER MAYOR O IGUAL A 4</a><br />
+            <asp:TextBox ID="txtNum1" type="number" runat="server"></asp:TextBox><br />
+            <asp:Button ID="btnCal" runat="server" Text="VER FIBONACCI" OnClick="btnCal_Click" /><br />
+            <asp:Label ID="lblRes1" runat="server" Text="LA SERIE ES: "></asp:Label><asp:Label ID="lblRes" runat="server" Text=""></asp:Label>
+            
+        </div>
+    </form>
+</body>
+</html>
---
+++ C# SOLO LOS ARCHIVOS IMPORTANTES/WebForm1.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FIBONA
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnCal_Click(object sender, EventArgs e)
+        {
+			//int n1 = Convert.ToInt32(txtNum1.Text.ToString().Trim());
+			//int cantidad = Convert.ToInt32(txtNum1.Text.ToString().Trim());
+            int n1 = 0, n2 = 1, n3, i, number;
+            string a = "";
+            //Console.Write("Enter the number of elements: ");
+            number = int.Parse(txtNum1.Text.ToString().Trim());
+            number = number - 1;
+            lblRes1.Text = n1 + " " + n1 + " " + n2 + " "; //printing 0 and 1    
+            for (i = 2; i < number; ++i) //loop starts from 2 because 0 and 1 are already printed    
+            {
+                n3 = n1 + n2;
+                a = a + " " + n3.ToString() ;
+                n1 = n2;
+                n2 = n3;
+            }
+            //lblRes1.Text = n1 + " " + n1 + " " + n2 + " ";
+            lblRes.Text = a;
+        }
+    }
+}
